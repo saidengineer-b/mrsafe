2526022657c8b6cb6529b0ae8788c12c72ef9d1c
Saeed <saidengineer@hotmail.com>
2025-06-24 22:14:03 +0300

    Fix WSGI module path to use mrsafe_project.settings

--- mrsafe_project/wsgi.py
+++ mrsafe_project/wsgi.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mrsafe_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mrsafe_app.settings')
+
 
 application = get_wsgi_application()
 
