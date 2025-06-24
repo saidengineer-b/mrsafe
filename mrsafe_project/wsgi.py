bae11233aa7815f085d560136197b985f44cbf3b
Saeed <saidengineer@hotmail.com>
2025-06-24 22:35:31 +0300

    ✅ Fixed WSGI module path to correct settings

--- mrsafe_project/wsgi.py
+++ mrsafe_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quiz_project project.
+WSGI config for mrsafe_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,14 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-# quiz_project/wsgi.py
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mrsafe_app.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mrsafe_project.settings')
 
 application = get_wsgi_application()
-
-
