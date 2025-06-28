d5449f286842ef808986f4c31e1a3cdaba4b7be0
Saeed <saidengineer@hotmail.com>
2025-06-28 19:58:50 +0300

    Fix root URL mapping for create-superuser

--- mrsafe_app/views/views_files.py
+++ mrsafe_app/views/views_files.py
@@ -318,7 +318,7 @@ from ..models import (
 
 
 @login_required
-@user_passes_test(is_admin)
+
 def admin_dashboard(request):
     """
     ✅ Mr. Safe Admin Dashboard: View statistics, manage users, courses, store, and premium plans.
